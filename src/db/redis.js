f477a34108694d8e2f72d60eed2fe2ab194ab5cd
unknown <hahvnkl0@gmail.com>
2023-11-30 15:05:50 +0200

    fd

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,6 +1,12 @@
 const {createClient} = require("redis");
 
-const redisClient = createClient();
+const redisClient = createClient({
+    legacyMode:true,
+    socket:{
+        host: process.env.REDIS_HOST,
+        port:process.env.REDIS_PORT
+    }
+})
 
 redisClient.on('error', err => console.log('Redis Client Error', err));
 redisClient.connect().then(()=>{console.log("redis connected");})
