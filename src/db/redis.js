380950555fdad5e6e7e2540bea76098d39213c99
unknown <hahvnkl0@gmail.com>
2023-12-04 15:09:38 +0200

    adding puppeteker

--- src/db/redis.js
+++ src/db/redis.js
@@ -3,8 +3,8 @@ const {createClient} = require("redis");
 const redisClient = createClient({
 
     socket:{
-        host: process.env.REDIS_HOST,
-        port:process.env.REDIS_PORT,
+        // host: process.env.REDIS_HOST,
+        // port:process.env.REDIS_PORT,
     }
     
 })
